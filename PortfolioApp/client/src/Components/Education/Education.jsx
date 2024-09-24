491c78836c82a72c58421138083f3b4668f07046
Ivan Gjorcev <gjorcev.ivan@gmail.com>
2024-09-24 23:36:35 +0200

    Update

--- PortfolioApp/client/src/Components/Education/Education.jsx
+++ PortfolioApp/client/src/Components/Education/Education.jsx
@@ -83,7 +83,7 @@ const Education = () => {
                   rel="noopener noreferrer"
                   href={item.educationlink}
                 >
-                  Work link <HiOutlineExternalLink size={18}/>
+                  School link <HiOutlineExternalLink size={18}/>
                 </a>
               </div>
             </VerticalTimelineElement>
