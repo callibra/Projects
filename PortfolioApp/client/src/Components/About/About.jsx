9b571b50f03db9d6dfdb46a7f1d1ba02a49f8ba1
Ivan Gjorcev <gjorcev.ivan@gmail.com>
2024-10-07 00:04:22 +0200

    Update

--- PortfolioApp/client/src/Components/About/About.jsx
+++ PortfolioApp/client/src/Components/About/About.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./About.css";
 import ProfilePic from "../../image/a.jpg"
+import { RiGithubFill, RiLinkedinFill, RiMoneyDollarCircleFill, RiSendPlaneFill  } from "react-icons/ri";
 import { Fade } from "react-awesome-reveal";
 
 const About = () => {
@@ -30,16 +31,37 @@ const About = () => {
             {/* <span className="colored-word">• Sit</span> doloremque quam aspernatur perferendis nobis placeat quis? Modi laborum deleniti reprehenderit aperiam velit harum dolorum facere repellat natus tenetur! Eligendi, pariatur! */}
             </p>
             <hr className="line-hr"></hr>
-            <h3 className="additional-title">My Passion for Coding !!!</h3>
+             {/* <h3 className="additional-title">My Passion for Coding !!!</h3> */}
+             <div className="button-container">
+                 <a href="https://github.com/" target="_blank" rel="noopener noreferrer" className="button github-button">
+                  <RiGithubFill size={20} style={{ marginRight: '5px' }} /> GitHub
+                </a>
+                <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer" className="button linkedin-button">
+                  <RiLinkedinFill size={20} style={{ marginRight: '5px' }}/> LinkedIn
+                </a>
+                <a href="https://www.upwork.com/" target="_blank" rel="noopener noreferrer" className="button upwork-button">
+                  <RiMoneyDollarCircleFill size={20} style={{ marginRight: '5px' }}/> Upwork
+               </a>
+               <a href="mailto:your-email@example.com" className="button gmail-button">
+                 <RiSendPlaneFill size={20} style={{ marginRight: '5px' }}/> Gmail
+               </a>
+             </div>
+            </div>
           </div>
         </div>
-      </div>
       </Fade>
     </div>
   );
 };
 
 export default About;
+
+
+
+
+
+
+
 
 
 
